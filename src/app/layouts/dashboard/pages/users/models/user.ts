a34333f0705c338343f0430a32e9f3725b0be8c9
Alexandra <alexa.dayanna07@gmail.com>
2024-01-22 23:24:46 -0500

    feat: the user interface was created

---
+++ src/app/layouts/dashboard/pages/users/models/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id: number,
+    firstName: string;
+    lastName: string;
+    gender: string;
+    email: string;
+    dateOfBirth: string;
+    magicWandWood: string;
+    magicWandCore: string;
+    magicWandLength: string;
+}
