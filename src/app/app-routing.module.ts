03fb27026717151830bdf93bc68522ec53c2024a
Alexandra <alexa.dayanna07@gmail.com>
2024-02-09 03:07:23 -0500

    feat: dashboard and not found paths were incorporated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './layouts/dashboard/dashboard.component';
+import { NotFoundComponent } from './layouts/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    loadChildren: () =>
+      import('./layouts/dashboard/dashboard.module').then((m) => m.DashboardModule),
+  },
+  {
+    path: '404',
+    component: NotFoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
