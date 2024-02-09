c78f8bc80a24e7196a8947d95bd68a53949fe74c
Alexandra <alexa.dayanna07@gmail.com>
2024-02-09 03:19:16 -0500

    feat: modules are incorporated

--- src/app/layouts/auth/auth.module.ts
+++ src/app/layouts/auth/auth.module.ts
@@ -1,15 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthComponent } from './auth.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AuthRoutingModule } from './auth-routing.module';
+import { SharedModule } from '../../shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-
+// Angular material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AuthComponent
+    LoginComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    AuthRoutingModule, 
+    SharedModule,
+    FormsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
 })
 export class AuthModule { }
