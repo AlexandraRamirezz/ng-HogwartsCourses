f31de322900d82c0b9d8586849a0b85232b90299
Alexandra <alexa.dayanna07@gmail.com>
2024-02-09 04:16:45 -0500

    feat: the enrollment interface was created

---
+++ src/app/layouts/dashboard/pages/enrollments/models/enrollment.ts
@@ -0,0 +1,9 @@
+export interface Enrollment {
+    enrollmentId: number;
+    courseId: number;
+    courseTitle: string;
+    studentId: number;
+    studentName: string;
+    shift: string;
+    modality: string;
+}
