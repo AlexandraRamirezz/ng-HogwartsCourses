e2cba2805b792a0e047359ead867e14c8b4805ef
Alexandra <alexa.dayanna07@gmail.com>
2024-02-09 03:53:26 -0500

    feat: added role property to users

--- src/app/layouts/dashboard/pages/users/models/user.ts
+++ src/app/layouts/dashboard/pages/users/models/user.ts
@@ -1,10 +1,11 @@
 export interface User {
-    id: number,
+    userId: number,
     firstName: string;
     lastName: string;
     gender: string;
     email: string;
     dateOfBirth: string;
+    role: string;
     magicWandWood: string;
     magicWandCore: string;
     magicWandLength: string;
