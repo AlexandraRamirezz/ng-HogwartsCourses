f7a8ab7ab679564bee5d59941e302f33f4cbe3e8
Alexandra <alexa.dayanna07@gmail.com>
2024-02-09 03:44:20 -0500

    feat: added the providers of users, enrollments and courses

--- src/app/layouts/dashboard/pages/courses/courses.module.ts
+++ src/app/layouts/dashboard/pages/courses/courses.module.ts
@@ -1,17 +1,55 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursesComponent } from './courses.component';
+import { CourseDialogComponent } from './components/course-dialog/course-dialog.component';
+import { SharedModule } from '../../../../shared/shared.module';
+import { RouterModule } from '@angular/router';
+import { CoursesRoutingModule } from './courses-routing.module';
+import { CoursesService } from '../../../../core/courses.service';
+import { EnrollmentsService } from '../../../../core/enrollments.service';
+import { UsersService } from '../../../../core/users.service';
 
+// Angular material
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
-    CoursesComponent
+    CoursesComponent,
+    CourseDialogComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CoursesRoutingModule,
+    SharedModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    RouterModule,
   ],
-  exports: [
-    CoursesComponent
-  ]
+  exports: [CoursesComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [
+    UsersService,
+    CoursesService,
+    EnrollmentsService
+  ],
 })
 export class CoursesModule { }
