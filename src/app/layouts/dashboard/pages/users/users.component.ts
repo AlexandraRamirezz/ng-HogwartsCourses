6395540ef69b6f40af1a454b77acaacdef8117c5
Alexandra <alexa.dayanna07@gmail.com>
2024-02-09 03:54:09 -0500

    feat: users' ABM methods were edited

--- src/app/layouts/dashboard/pages/users/users.component.ts
+++ src/app/layouts/dashboard/pages/users/users.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { UserDialogComponent } from './components/user-dialog/user-dialog.component';
 import { User } from './models/user';
-import { EventEmitter, Input, Output } from '@angular/core';
+import { UsersService } from '../../../../core/users.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-users',
@@ -10,122 +11,98 @@ import { EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent {
-  userName = '';
   style = 'bolder';
 
-  users: User[] = [
-    {
-      id: 1,
-      firstName: 'Diego',
-      lastName: 'Defilippi',
-      gender: 'Male',
-      email: 'diego.defilippi@gmail.com',
-      dateOfBirth: '10/02/2003',
-      magicWandWood: 'Oak',
-      magicWandCore: 'Phoenix feather',
-      magicWandLength: 'Long',
-    },
-    {
-      id: 2,
-      firstName: 'Alexandra',
-      lastName: 'Ramirez',
-      gender: 'Female',
-      email: 'alexandra.ramirez@gmail.com',
-      dateOfBirth: '03/07/2004',
-      magicWandWood: 'Cherry tree',
-      magicWandCore: 'Unicorn hair',
-      magicWandLength: 'Short',
-    },
-    {
-      id: 3,
-      firstName: 'Elena',
-      lastName: 'García',
-      gender: 'Female',
-      email: 'elena.garcia@gmail.com',
-      dateOfBirth: '05/15/1990',
-      magicWandWood: 'Willow',
-      magicWandCore: 'Unicorn hair',
-      magicWandLength: 'Long',
-    },
-    {
-      id: 4,
-      firstName: 'Carlos',
-      lastName: 'López',
-      gender: 'Male',
-      email: 'carlos.lopez@gmail.com',
-      dateOfBirth: '12/30/1985',
-      magicWandWood: 'Walnut',
-      magicWandCore: "Dragon's heartstring",
-      magicWandLength: 'Short',
-    },  
-    {
-      id: 5,
-      firstName: 'Isabella',
-      lastName: 'Rodríguez',
-      gender: 'Female',
-      email: 'isabella.rodriguez@gmail.com',
-      dateOfBirth: '08/21/2000',
-      magicWandWood: 'Cherry tree',
-      magicWandCore: 'Phoenix feather',
-      magicWandLength: 'Medium',
-    },
-  ];
+  displayedColumns: string[] = ['userId', 'fullName', 'gender', 'email', 'dateOfBirth', 'role', 'magicWandWood', 'magicWandCore','magicWandLength', 'actions'];
+  
+  users: User[] = [];
 
-  constructor(private matDialog: MatDialog) { }
-
-  openUserDialog(): void {
-    this.matDialog
-      .open(UserDialogComponent)
-      .afterClosed()
-      .subscribe({
-        next: (v) => {
-          if (!!v) {
-            const maxId = Math.max(...this.users.map(user => user.id), 0);
-            const newId = maxId + 1;
-
-            this.users = [
-              ...this.users,
-              {
-                ...v,
-                id: newId,
-              },
-            ];
-          }
-        },
-      });
+  constructor(private matDialog: MatDialog,private usersService: UsersService) {
+    this.usersService.getUsers().subscribe({
+      next: (user) => {
+        this.users = user;
+      }
+    })
   }
 
-  onEditUser(user: User): void {
+  onCreateUser(): void {
     this.matDialog
       .open(UserDialogComponent, {
-        data: user,
-      })
-      .afterClosed()
-      .subscribe({
-        next: (v) => {
-          if (!!v) {
-            this.users = this.users.map((u) =>
-              u.id === user.id ? { ...u, ...v } : u
-            );
+        data: { view: false, edit: false }
+      }).afterClosed().subscribe({
+        next: (result) => {
+          if (result) {
+            const maxId = Math.max(...this.users.map(user => user.userId), 0);
+            const newId = maxId + 1;
+            const newUser = {
+              ...result,
+              userId: newId,
+            };
+  
+            this.usersService.addUsers(newUser).subscribe({
+              next: (us) => {
+                this.users = us;
+              },
+            });
           }
-        },
+        }
       });
+  }  
+
+  onEditUser(user: User) {
+    this.matDialog
+      .open(UserDialogComponent, {
+      data: { user: user, view: false, edit: true }
+    }).afterClosed().subscribe({
+      next: (result) => {
+        if (result) {
+          this.usersService.updateUsers(user.userId, result).subscribe({
+            next: (us) => (this.users = us),
+          })
+        }
+      }
+    })
   }
 
-  onDeleteUser(userId: number): void {
-    if (confirm('Do you want to delete the student permanently?')) {
-      this.users = this.users.filter((u) => u.id !== userId);
-    }
+  onViewUser(user: User) {
+    this.matDialog
+      .open(UserDialogComponent, {
+      data: { user: user, view: true, edit: false }
+    })
   }
 
-  @Input()
-  dataSource: User[] = [];
-
-  @Output()
-  deleteUser = new EventEmitter<number>();
-
-  @Output()
-  editUser = new EventEmitter<User>();
-
-  displayedColumns = ['id', 'fullName', 'email', 'dateOfBirth', 'magicWandWood', 'magicWandCore','magicWandLength', 'actions'];
+  onDeleteUser(userId: number) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'This action cannot be reversed',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete',
+      cancelButtonText: 'Cancel'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.usersService.deleteUsersByID(userId).subscribe({
+          next: (us) => {
+            this.users = us;
+            Swal.fire({
+              icon: 'success',
+              title: 'User successfully deleted',
+              showConfirmButton: false,
+              timer: 1500
+            });
+          },
+          error: (error) => {
+            console.error('Error deleting user: ', error);
+            Swal.fire({
+              icon: 'error',
+              title: 'Error',
+              text: 'There was an error deleting the user.'
+            });
+          }
+        });
+      }
+    });
+  }
 }
