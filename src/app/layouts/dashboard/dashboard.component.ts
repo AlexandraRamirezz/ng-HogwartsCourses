8a3af3297fea97700bb0a7a3ec6277059d969d7e
Alexandra <alexa.dayanna07@gmail.com>
2024-02-09 04:27:01 -0500

    feat: modules have been added to the dashboard and the mat-list-item of students is changed to users

--- src/app/layouts/dashboard/dashboard.component.ts
+++ src/app/layouts/dashboard/dashboard.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
-  showSidebar = true;
-  style = 'bolder';
+export class DashboardComponent {  
+  showSidebar = false;
 }
