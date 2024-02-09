337fbeff6d7ff37e0dffcaad71c16702a2994468
Alexandra <alexa.dayanna07@gmail.com>
2024-02-09 03:01:01 -0500

    feat: the not found module and component was created

---
+++ src/app/layouts/not-found/not-found.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.scss'
+})
+export class NotFoundComponent { }
