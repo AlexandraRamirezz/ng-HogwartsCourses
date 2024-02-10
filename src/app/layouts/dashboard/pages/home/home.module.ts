bd7fc9e3a2606e60dce2a1520a3b9218d651ef8a
Alexandra <alexa.dayanna07@gmail.com>
2024-02-09 19:23:59 -0500

    feat: home routing was created

--- src/app/layouts/dashboard/pages/home/home.module.ts
+++ src/app/layouts/dashboard/pages/home/home.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { HomeRoutingModule } from './home-routing.module';
 
 
 @NgModule({
@@ -8,7 +9,8 @@ import { HomeComponent } from './home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HomeRoutingModule
   ]
 })
 export class HomeModule { }
