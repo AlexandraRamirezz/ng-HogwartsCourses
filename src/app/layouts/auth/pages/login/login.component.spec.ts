8e80fe4b1f0880dfcd31930b1eb55f0ab57a3f57
Alexandra <alexa.dayanna07@gmail.com>
2024-02-18 02:09:17 -0500

    feat: added login design and spect

---
+++ src/app/layouts/auth/pages/login/login.component.spec.ts
@@ -0,0 +1,46 @@
+import { TestBed } from "@angular/core/testing";
+import { LoginComponent } from "./login.component";
+import { AuthService } from "../../../../core/services/auth.service";
+import { CommonModule } from '@angular/common';
+import { AuthRoutingModule } from '../../auth-routing.module';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
+import { MockProvider } from 'ng-mocks';
+import { SharedModule } from "../../../../shared/shared.module";
+
+describe('LoginComponent', () => {
+    let component: LoginComponent;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [LoginComponent],
+            providers: [MockProvider(AuthService)],
+            imports: [
+                CommonModule,
+                AuthRoutingModule,
+                SharedModule,
+                ReactiveFormsModule,
+            ],
+        })
+        component = TestBed.createComponent(LoginComponent).componentInstance;
+    });
+
+    it('The LoginComponent is successfully instantiated', () => {
+        expect(component).toBeTruthy()
+    })
+
+    it('Email and password should be required', () => {
+        expect(component.loginForm.get('email')?.hasValidator(Validators.required)).toBeTrue();
+        expect(component.loginForm.get('password')?.hasValidator(Validators.required)).toBeTrue();
+    })
+
+    it('Invalid form test with fields in touched', () => {
+        const spyOnMarkAllAsTouched = spyOn(component.loginForm,'markAllAsTouched');
+        component.loginForm.patchValue({
+            email: '',
+            password: '',
+        });
+        expect(component.loginForm.invalid).toBeTrue();
+        component.onSubmit();
+        expect(spyOnMarkAllAsTouched).toHaveBeenCalled();
+    })
+})
--- src/app/layouts/auth/pages/login/login.component.ts
+++ src/app/layouts/auth/pages/login/login.component.ts
@@ -1,8 +1,45 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../../core/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent { }
+export class LoginComponent {
+  hide = true;
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    })
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      this.loginForm.get('email')?.setValue('');
+      this.loginForm.get('password')?.setValue('');
+    } else {
+      const email = this.loginForm.get('email')?.value;
+      const password = this.loginForm.get('password')?.value;
+      this.authService.login(this.loginForm.value);
+    }
+  }
+
+  showErrorMessage(message: string) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: message,
+    });
+  }
+
+  togglePasswordVisibility() {
+    this.hide = !this.hide;
+  }
+}
