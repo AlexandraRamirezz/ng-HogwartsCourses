e6939a5ae355f496e0462dd8ab851f04184fedf6
Alexandra <alexa.dayanna07@gmail.com>
2024-02-18 02:13:45 -0500

    fix: changed the pathMatch and added the SharedModule

--- src/app/layouts/auth/auth-routing.module.ts
+++ src/app/layouts/auth/auth-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   },
   {
     path:'',
-    redirectTo: 'login', pathMatch:'full',
+    redirectTo: 'login', pathMatch: 'full',
   }
 ];
 
--- src/app/layouts/auth/auth.module.ts
+++ src/app/layouts/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { SharedModule } from '../../shared/shared.module';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule, 
+    AuthRoutingModule,
     SharedModule,
   ],
 })
