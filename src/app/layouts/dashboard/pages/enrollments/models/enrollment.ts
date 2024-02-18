6bb40e6f8faab9ae6c1538b48b09109525796051
Alexandra <alexa.dayanna07@gmail.com>
2024-02-18 03:10:03 -0500

    feat: id, startDate, endDate, teacher, email, and magicWandCore fields are added to the interface

--- src/app/layouts/dashboard/pages/enrollments/models/enrollment.ts
+++ src/app/layouts/dashboard/pages/enrollments/models/enrollment.ts
@@ -1,9 +1,15 @@
 export interface Enrollment {
-    enrollmentId: number;
-    courseId: number;
+    id: string;
     courseTitle: string;
-    studentId: number;
     studentName: string;
+    startDate: Date;
+    endDate: Date;
     shift: string;
     modality: string;
+    teacher: string,
+    email: string,
+    magicWandCore: string,
+    enrollmentId: number;
+    studentId: number;
+    courseId: number;
 }
