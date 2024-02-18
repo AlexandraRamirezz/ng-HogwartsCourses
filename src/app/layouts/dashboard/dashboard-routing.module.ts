e306ade26530a2f1aeb98e7e20775128f372e2a1
Alexandra <alexa.dayanna07@gmail.com>
2024-02-18 03:24:16 -0500

    feat: component title data is added

--- src/app/layouts/dashboard/dashboard-routing.module.ts
+++ src/app/layouts/dashboard/dashboard-routing.module.ts
@@ -5,20 +5,24 @@ const routes: Routes = [
   {
     path: 'users',
     loadChildren: () => import('./pages/users/users.module').then((m) => m.UsersModule),
+    data: { title: 'List of users' },
   },
   {
     path: 'courses',
     loadChildren: () => import('./pages/courses/courses.module').then((m) => m.CoursesModule),
+    data: { title: 'List of courses' },
   },
   {
     path: 'enrollments',
     loadChildren: () => import('./pages/enrollments/enrollments.module').then((m) => m.EnrollmentsModule),
+    data: { title: 'List of enrollments' },
   },
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then((m) => m.HomeModule),
+    data: { title: 'Home' },
   },
-  { 
+  {
     path: "",
     redirectTo: "home", pathMatch: "prefix" 
   },
