0bc3b916e1928ceca4dbf1cf2997b086f8c71c1f
Alexandra <alexa.dayanna07@gmail.com>
2024-02-18 02:16:48 -0500

    feat: added logout validation for the link

--- src/app/layouts/dashboard/components/sidebar/sidebar.component.ts
+++ src/app/layouts/dashboard/components/sidebar/sidebar.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../../../../core/services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent { 
-  constructor(private router: Router){}
-  logout(): void{
-    this.router.navigate(['auth', 'login'])
-  }
+export class SidebarComponent {
+  constructor(private router: Router) {}
+    logout(): void{
+      this.router.navigate(['auth', 'login'])
+    }
 }
