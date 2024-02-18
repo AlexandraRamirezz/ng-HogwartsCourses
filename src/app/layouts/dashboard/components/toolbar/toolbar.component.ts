cc2db0a12ab7970d1ecc32fd838f8cd93439a40d
Alexandra <alexa.dayanna07@gmail.com>
2024-02-18 02:18:23 -0500

    feat: added the title, the userName with its logo and the logout button

--- src/app/layouts/dashboard/components/toolbar/toolbar.component.ts
+++ src/app/layouts/dashboard/components/toolbar/toolbar.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 import { Output } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { AuthService } from '../../../../core/services/auth.service';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-toolbar',
@@ -8,6 +11,37 @@ import { Output } from '@angular/core';
   styleUrl: './toolbar.component.scss'
 })
 export class ToolbarComponent {
+  userName: string | undefined;
+  authUser: any;
+  title: string = 'Home';
+
+  constructor(private router: Router, private authService: AuthService, private activatedRoute: ActivatedRoute) {}
+  ngOnInit():void {
+    this.userName = this.authService.authUser?.firstName + " " + this.authService.authUser?.lastName;
+    this.authUser = this.authService.authUser;
+    
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => {
+        let route = this.activatedRoute;
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
+        return route;
+      }),
+      filter(route => route.outlet === 'primary'),
+      map(route => route.snapshot.data['title'])
+    ).subscribe(title => {
+      this.title = title;
+      console.log(this.title);
+    });
+    
+  }
+
+  logout(): void{
+    this.authService.logout();
+  }
+
   @Output()
   toggleSidebar = new EventEmitter();
 }
