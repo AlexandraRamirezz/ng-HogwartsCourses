3c8ce03d95bb90b7c6da68e0668bd11236289e82
Alexandra <alexa.dayanna07@gmail.com>
2024-02-18 02:41:33 -0500

    feat: password and id fields are added to the interface

--- src/app/layouts/dashboard/pages/users/models/user.ts
+++ src/app/layouts/dashboard/pages/users/models/user.ts
@@ -1,10 +1,12 @@
 export interface User {
-    userId: number,
+    id: string;
     firstName: string;
     lastName: string;
     gender: string;
     email: string;
+    password: string;
     dateOfBirth: string;
     magicWandCore: string;
     role: string;
+    userId: number,
 }
