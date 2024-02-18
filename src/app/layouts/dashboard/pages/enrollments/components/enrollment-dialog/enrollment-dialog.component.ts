9bfb3f8f71d9e0088d7f87484fa48a40d6c69cef
Alexandra <alexa.dayanna07@gmail.com>
2024-02-18 03:05:52 -0500

    feat: added the fields of the start date, end date, teacher, user email, and user's core of magic wand to get the data in the table.

--- src/app/layouts/dashboard/pages/enrollments/components/enrollment-dialog/enrollment-dialog.component.ts
+++ src/app/layouts/dashboard/pages/enrollments/components/enrollment-dialog/enrollment-dialog.component.ts
@@ -22,13 +22,17 @@ export class EnrollmentDialogComponent {
 
     @Inject(MAT_DIALOG_DATA) private data: { enrollment: Enrollment, view: boolean, edit: boolean },
     private coursesService: CoursesService,
-    private usersService: UsersService,
-    ){
+    private usersService: UsersService) {
     this.enrollmentForm = this.fb.group({
       courseTitle: ['', [Validators.required]],
       studentName: ['', [Validators.required]],
+      startDate: [{ value: '', disabled: true }, [Validators.required]],
+      endDate: [{ value: '', disabled: true }, [Validators.required]],
       shift: [{ value: '', disabled: true }, [Validators.required, Validators.pattern('^[a-zA-ZáéíóúÁÉÍÓÚüÜ ]+$')]],
       modality: [{ value: '', disabled: true }, [Validators.required, Validators.pattern('^[a-zA-ZáéíóúÁÉÍÓÚüÜ ]+$')]],
+      teacher: [{ value: '', disabled: true }, [Validators.required, Validators.pattern('^[a-zA-ZáéíóúÁÉÍÓÚüÜ ]+$')]],
+      email: [{ value: '', disabled: true }, [Validators.required, Validators.pattern('^[a-zA-ZáéíóúÁÉÍÓÚüÜ ]+$')]],
+      magicWandCore: [{ value: '', disabled: true }, [Validators.required, Validators.pattern('^[a-zA-ZáéíóúÁÉÍÓÚüÜ ]+$')]],
     });
     if (this.data.view) {
       this.enrollmentForm.patchValue(this.data.enrollment);
@@ -44,19 +48,35 @@ export class EnrollmentDialogComponent {
     this.coursesService.getCourses().subscribe(courses => {
       this.courses = courses;
     });
-
-    this.usersService.getUsers().subscribe(users => {
-      this.users = users;
+    
+    this.usersService.getStudents().subscribe(students => {
+      this.users = students;
     })
 
     this.enrollmentForm?.get('courseTitle')?.valueChanges.subscribe(courseTitle => {
       const selectedCourse = this.courses.find(course => course.title === courseTitle);
       if (selectedCourse) {
+        this.enrollmentForm?.get('startDate')?.setValue(selectedCourse.startDate);
+        this.enrollmentForm?.get('endDate')?.setValue(selectedCourse.endDate);
         this.enrollmentForm?.get('shift')?.setValue(selectedCourse.shift);
         this.enrollmentForm?.get('modality')?.setValue(selectedCourse.modality);
+        this.enrollmentForm?.get('teacher')?.setValue(selectedCourse.teacher);
       } else {
-        this.enrollmentForm?.get('shift')?.setValue('');
-        this.enrollmentForm?.get('modality')?.setValue('');
+        this.enrollmentForm?.get('startDate')?.setValue(selectedCourse.startDate);
+        this.enrollmentForm?.get('endDate')?.setValue(selectedCourse.endDate);
+        this.enrollmentForm?.get('shift')?.setValue(selectedCourse.shift);
+        this.enrollmentForm?.get('modality')?.setValue(selectedCourse.modality);
+        this.enrollmentForm?.get('teacher')?.setValue(selectedCourse.teacher);
+      }
+    });
+    this.enrollmentForm?.get('studentName')?.valueChanges.subscribe(studentName => {
+      const selectedUser = this.users.find(user => (user.firstName + " " + user.lastName) === studentName);
+      if (selectedUser) {
+        this.enrollmentForm?.get('email')?.setValue(selectedUser.email);
+        this.enrollmentForm?.get('magicWandCore')?.setValue(selectedUser.magicWandCore);
+      } else {
+        this.enrollmentForm?.get('email')?.setValue(selectedUser.email);
+        this.enrollmentForm?.get('magicWandCore')?.setValue(selectedUser.magicWandCore);
       }
     });
   }
@@ -67,13 +87,23 @@ export class EnrollmentDialogComponent {
       this.showErrorMessage('Please fill in all fields correctly.');
       return;
     }
+    this.enrollmentForm.get('startDate')?.enable();
+    this.enrollmentForm.get('endDate')?.enable();
     this.enrollmentForm.get('shift')?.enable();
     this.enrollmentForm.get('modality')?.enable();
+    this.enrollmentForm.get('teacher')?.enable();
+    this.enrollmentForm.get('email')?.enable();
+    this.enrollmentForm.get('magicWandCore')?.enable();
 
     this.matDialogRef.close(this.enrollmentForm.value);
 
+    this.enrollmentForm.get('startDate')?.disable();
+    this.enrollmentForm.get('endDate')?.disable();
     this.enrollmentForm.get('shift')?.disable();
     this.enrollmentForm.get('modality')?.disable();
+    this.enrollmentForm.get('email')?.disable();
+    this.enrollmentForm.get('modality')?.disable();
+    this.enrollmentForm.get('magicWandCore')?.disable();
   }
 
   showErrorMessage(message: string) {
@@ -81,9 +111,9 @@ export class EnrollmentDialogComponent {
       icon: 'error',
       title: 'Error',
       text: message,
-      confirmButtonColor: '#1e88e5',
+      confirmButtonColor: '#ef5350',
       background: '#303030',
-      color: 'white',
+      color: '#d0cccc',
     });
   }
 
